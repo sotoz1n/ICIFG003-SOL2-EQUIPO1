0a36b7581490bb50a412007a49c60445e07b29bd
Franco029 <frankosepulvedar@gmail.com>
2026-06-21 23:50:50 -0400

    Se agrgan productos a la tienda

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -10,19 +10,39 @@ WHERE NOT EXISTS (SELECT 1 FROM cliente WHERE id = 2);
 
 -- Inserción previa de Categorías obligatoria para evitar fallos de llave foránea en productos
 INSERT INTO categoria_producto (id, nombre_categoria, descripcion)
-SELECT 1, 'Alimentos', 'Comida para perros, gatos y mascotas en general'
+SELECT 1, 'Perros', 'Comida para perros, juguetes y accesorios para perros'
 WHERE NOT EXISTS (SELECT 1 FROM categoria_producto WHERE id = 1);
 
 INSERT INTO categoria_producto (id, nombre_categoria, descripcion)
 SELECT 2, 'Accesorios', 'Juguetes, collares, correas y camas'
 WHERE NOT EXISTS (SELECT 1 FROM categoria_producto WHERE id = 2);
 
+INSERT INTO categoria_producto (id, nombre_categoria, descripcion)
+SELECT 3, 'Gatos', 'Juguetes, alimento y accesorios para gatos'
+WHERE NOT EXISTS (SELECT 1 FROM categoria_producto WHERE id = 3);
 
+--CATEGORIA 1: PERRO, 2: ACCESORIOS, 3: GATO
 -- Inserción de Productos iniciales adaptado a tus columnas reales
 INSERT INTO producto (id, nombre, descripcion, precio, stock, imagen, id_categoria) 
-SELECT 1, 'Alimento Perro Adulto 15kg', 'Nutrición completa para mascotas adultas', 35990.00, 50, 'https://unimarc.vtexassets.com/arquivos/ids/246947/000000000000168870-UN-01.jpg.jpg?v=638755113566130000', 1
+SELECT 1, 'Alimento Perro Adulto 15kg', 'Nutrición completa para mascotas adultas', 25990, 50, 'https://unimarc.vtexassets.com/arquivos/ids/244816/000000000000231126-UN-01.jpg?v=638652959508770000', 1
 WHERE NOT EXISTS (SELECT 1 FROM producto WHERE id = 1);
 
 INSERT INTO producto (id, nombre, descripcion, precio, stock, imagen, id_categoria) 
-SELECT 2, 'Collar Ajustable Reflectante', 'Collar de alta visibilidad nocturna para paseos', 7990.00, 30, 'https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwd1d829d0/images/8941_m1.jpg', 2
+SELECT 2, 'Collar Ajustable Reflectante', 'Collar de alta visibilidad nocturna para paseos', 7990, 30, 'https://ae01.alicdn.com/kf/S191f195dad3a49a09ea2493654554625d.jpg?has_lang=1&ver=2', 2
 WHERE NOT EXISTS (SELECT 1 FROM producto WHERE id = 2);
+
+INSERT INTO producto (id, nombre, descripcion, precio, stock, imagen, id_categoria) 
+SELECT 3, 'Casa Para Perro', 'Casa cómoda y segura para tu mascota', 69990, 10, 'https://easycl.vteximg.com.br/arquivos/ids/4869600/1389960-0000-001.jpg?v=638863619620530000', 1
+WHERE NOT EXISTS (SELECT 1 FROM producto WHERE id = 3);
+
+INSERT INTO producto (id, nombre, descripcion, precio, stock, imagen, id_categoria) 
+SELECT 4, 'Juguete Raton', 'Juguete interactivo para gatos', 4990, 60, 'https://arenaparamascotas.cl/wp-content/uploads/2024/09/Juguete-Raton-con-sonido-para-gatos-2.jpg', 3
+WHERE NOT EXISTS (SELECT 1 FROM producto WHERE id = 4);
+
+INSERT INTO producto (id, nombre, descripcion, precio, stock, imagen, id_categoria) 
+SELECT 5, 'Arenero de Gato', 'Arenero espacioso para gatos', 32990, 100, 'https://cl-cenco-pim-resizer.ecomm.cencosud.com/unsafe/adaptive-fit-in/3840x0/filters:quality(75)/prd-cl/product-medias/3ddd90c2-cb63-4397-a5d0-6e19ba3f0d4b/MKJEOA9OV5/MKJEOA9OV5-1/1763662113336-MKJEOA9OV5-1-1.jpg', 3
+WHERE NOT EXISTS (SELECT 1 FROM producto WHERE id = 5);
+
+INSERT INTO producto (id, nombre, descripcion, precio, stock, imagen, id_categoria) 
+SELECT 6, 'Peine para Mascotas', 'Peine para el cuidado del pelaje de tu mascota', 8990, 20, 'https://jumbocl.vteximg.com.br/arquivos/ids/339891-250-250/Accesorio-Mascota-Corta-U%C3%B1as.jpg?v=638776440296670000', 2
+WHERE NOT EXISTS (SELECT 1 FROM producto WHERE id = 6);
